96ced713a481ef9564f71def8700bcf18bfaa6e8
Raul Bolivar Navas <raul.bolivar@pragma.com.co>
2025-12-10 09:08:56 -0500

    adjust parameter store synchronous 1

--- infrastructure/adapter-parameter/src/main/java/com/demo/sd/sn/infrastructure/parameter/ParameterStoreAdapter.java
+++ infrastructure/adapter-parameter/src/main/java/com/demo/sd/sn/infrastructure/parameter/ParameterStoreAdapter.java
@@ -3,44 +3,57 @@ package com.demo.sd.sn.infrastructure.parameter;
 import com.demo.sd.sn.domain.port.out.OutParameterPort;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import software.amazon.awssdk.services.ssm.SsmAsyncClient;
+import software.amazon.awssdk.services.ssm.SsmClient;
 import software.amazon.awssdk.services.ssm.model.GetParameterRequest;
 
 @Slf4j
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ParameterStoreAdapter implements OutParameterPort {
 
-    private final SsmAsyncClient ssmAsyncClient;
+    private final SsmClient ssmClient;
+    private final Cache<String, String> cache;
     private final ObjectMapper mapper;
 
     @Override
-    public <T> Mono<T> getParameter(String parameterName, Class<T> clazz) {
+    public <T> T getParameter(String parameterName, Class<T> clazz) {
 
-        GetParameterRequest request = GetParameterRequest.builder()
-                .name(parameterName)
-                .withDecryption(true)   // !!! IMPORTANTE para SecureString !!!
-                .build();
+        String value = cache.get(parameterName, this::loadParameter);
 
-        return Mono.fromFuture(ssmAsyncClient.getParameter(request))
-                .map(response -> response.parameter().value())
-                .doOnSubscribe(unused ->
-                        log.info("Fetching parameter: {}", parameterName))
-                .doOnSuccess(unused ->
-                        log.info("Parameter fetched: {}", parameterName))
-                .flatMap(value -> convert(value, clazz))
-                .onErrorResume(ex -> {
-                    log.error("Error getting parameter {}", parameterName, ex);
-                    return Mono.error(ex);
-                });
+        return convert(value, clazz);
+    }
+
+    private String loadParameter(String parameterName) {
+        try {
+            log.info("[PARAMS] Loading parameter: {}", parameterName);
+
+            return ssmClient.getParameter(
+                    GetParameterRequest.builder()
+                            .name(parameterName)
+                            .withDecryption(true)
+                            .build()
+            ).parameter().value();
+
+        } catch (Exception e) {
+            log.error("[PARAMS] Error loading parameter {}", parameterName, e);
+            throw new IllegalStateException("Unable to load parameter: " + parameterName, e);
+        }
     }
 
     @SuppressWarnings("unchecked")
-    private <T> Mono<T> convert(String value, Class<T> clazz) {
-        if (clazz.equals(String.class)) return Mono.just((T) value);
-        return Mono.fromCallable(() -> mapper.readValue(value, clazz));
+    private <T> T convert(String value, Class<T> clazz) {
+        try {
+            if (clazz.equals(String.class)) {
+                return (T) value;
+            }
+            return mapper.readValue(value, clazz);
+        } catch (Exception e) {
+            throw new IllegalStateException("Error converting parameter value to " + clazz.getSimpleName(), e);
+        }
     }
 }
